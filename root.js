a078ae25e9023e467dd0c3a08e5a88232ecf625b
THEO DEGUFFROY KERBOEUF (Etudiant) <degufthe@ur.univ-rouen.fr>
2022-01-24 14:04:14 +0100

    correction super nickel

--- root.js
+++ root.js
@@ -8,7 +8,7 @@
 const fs = require('fs');
 const path = require('path');
 const url = require('url');
-//bug ok 
+//bug ok c'est super nickel
 // import {dirname} from 'path';
 // import {fileURLToPath} from 'url';
 
